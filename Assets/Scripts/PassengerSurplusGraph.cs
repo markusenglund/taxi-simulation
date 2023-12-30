5edf2de26a5f95d0ab76d946af8af009784413a3
markusenglund <markus.s.englund@gmail.com>
2023-12-30 12:45:23 +0100

    Fix of compiler error

--- Assets/Scripts/PassengerSurplusGraph.cs
+++ Assets/Scripts/PassengerSurplusGraph.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-private class Transaction
+public class Transaction
 {
     public float time { get; set; }
     public decimal amount { get; set; }
@@ -22,8 +22,8 @@ public class PassengerSurplusGraph : MonoBehaviour
     [SerializeField] private TMP_Text textPrefab;
     [SerializeField] private TMP_Text headerTextPrefab;
     [SerializeField] private TMP_Text legendTextPrefab;
-    
-    
+
+
 
     LineRenderer unservedLine;
     LineRenderer pickedUpLine;
@@ -35,7 +35,7 @@ public class PassengerSurplusGraph : MonoBehaviour
 
     int numUnservedPassengers = 0;
     int numPickedUpPassengers = 0;
-    
+
     float margin = 26f;
     float marginTop = 50f;
     float maxY = 50f;
@@ -96,9 +96,10 @@ public class PassengerSurplusGraph : MonoBehaviour
     }
 
 
-    private void CreateAxes() {
+    private void CreateAxes()
+    {
         // Create x axis with the line renderer
-    LineRenderer xLineRenderer = Instantiate(lrPrefab, graphContainer);
+        LineRenderer xLineRenderer = Instantiate(lrPrefab, graphContainer);
         xLineRenderer.positionCount = 2;
         Vector2 zeroPosition = ConvertValueToGraphPosition(new Vector2(0, 0));
         Vector2 maxXPosition = ConvertValueToGraphPosition(new Vector2(maxX, 0));
@@ -114,10 +115,12 @@ public class PassengerSurplusGraph : MonoBehaviour
 
     }
 
-    private void CreateAxisLabels() {
+    private void CreateAxisLabels()
+    {
         // Create y axis labels
         int step = Mathf.RoundToInt((maxY - minY) / 5f);
-        for (int i = (int)minY; i <= maxY; i += step) {
+        for (int i = (int)minY; i <= maxY; i += step)
+        {
             TMP_Text text = Instantiate(textPrefab, graphContainer);
             Vector2 textPosition = ConvertValueToGraphPosition(new Vector2(0, i));
             text.text = i.ToString();
@@ -125,14 +128,16 @@ public class PassengerSurplusGraph : MonoBehaviour
         }
     }
 
-    private void CreateHeaderText() {
+    private void CreateHeaderText()
+    {
         TMP_Text text = Instantiate(headerTextPrefab, graphContainer);
         Vector2 textPosition = new Vector2(-52f, 70f);
         text.text = "Passengers";
         text.rectTransform.anchoredPosition = textPosition;
     }
 
-    private void CreateLegend() {
+    private void CreateLegend()
+    {
         TMP_Text text1 = Instantiate(legendTextPrefab, graphContainer);
         Vector2 textPosition1 = new Vector2(80, 74f);
         text1.text = "Picked up";
@@ -180,7 +185,7 @@ public class PassengerSurplusGraph : MonoBehaviour
         // Set the color of picked up line to green
         pickedUpLine.startColor = new Color(0.0f, 1.0f, 0.0f, 1.0f);
         pickedUpLine.endColor = new Color(0.0f, 1.0f, 0.0f, 1.0f);
-        
+
         CreateAxes();
         CreateAxisLabels();
         CreateHeaderText();
@@ -200,13 +205,14 @@ public class PassengerSurplusGraph : MonoBehaviour
     }
 
 
-    private void CreateDot(Vector2 position) {
+    private void CreateDot(Vector2 position)
+    {
         Transform dot = Instantiate(dotPrefab, graphContainer);
         RectTransform rectTransform = dot.GetComponent<RectTransform>();
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
         rectTransform.anchoredPosition = new Vector3(position.x, position.y, -1);
-        
+
     }
 }
 
