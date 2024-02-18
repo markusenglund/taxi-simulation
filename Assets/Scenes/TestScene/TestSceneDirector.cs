f5a1210005276b785a198123c1a4ff9a51134a9c
markusenglund <markus.s.englund@gmail.com>
2024-02-18 20:46:54 +0100

    Create a passenger in the test project

--- Assets/Scenes/TestScene/TestSceneDirector.cs
+++ Assets/Scenes/TestScene/TestSceneDirector.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class TestSceneDirector : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] public Transform passengerPrefab;
+
     void Start()
     {
-        Debug.Log("Generating street grid");
-
         GridUtils.GenerateStreetGrid(null);
+        StartCoroutine(Scene());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Scene()
     {
+        yield return new WaitForSeconds(1);
+        Transform passenger = Instantiate(passengerPrefab, new Vector3(1.7f, 0.08f, 0.22f), Quaternion.identity);
+        Vector3 lookAt = Camera.main.transform.position;
+        lookAt.y = passenger.position.y;
+        passenger.LookAt(lookAt);
+        yield return new WaitForSeconds(1);
+        // Zoom out
 
+    }
+
+    public void ZoomOut(float distance, float duration)
+    {
+        // TODO: START HERE - Implement the ZoomOut method which is supposed to gradually zoom out based on "animateValue" in the primer project
     }
 }
