150d03a1677798f3ae29f3def4012dd683cf91f6
Markus Englund <markus@englund.dev>
2024-03-24 15:31:25 +0100

    Add second city at the end of rotating scene

--- Assets/Scenes/005_SlowRotatingScene/SlowRotatingSceneDirector.cs
+++ Assets/Scenes/005_SlowRotatingScene/SlowRotatingSceneDirector.cs
@@ -9,10 +9,13 @@ public class SlowRotatingScene : MonoBehaviour
     [SerializeField] public SimulationSettings simSettings;
     [SerializeField] public GraphSettings graphSettings;
     Vector3 middlePosition = new Vector3(4.5f, -2, 4.5f);
-    City city;
+    City city1;
+    City city2;
     void Awake()
     {
-        city = City.Create(cityPrefab, 0, 0, simSettings, graphSettings);
+        city1 = City.Create(cityPrefab, 0, 0, simSettings, graphSettings);
+        city2 = City.Create(cityPrefab, 0, -12, simSettings, graphSettings);
+        city2.transform.position = new Vector3(0, -100, -12);
     }
 
     void Start()
@@ -25,11 +28,34 @@ public class SlowRotatingScene : MonoBehaviour
     IEnumerator Scene()
     {
         Time.timeScale = 10;
-        yield return new WaitForSeconds(20);
+        yield return new WaitForSeconds(10);
         Time.timeScale = 1;
-        StartCoroutine(CameraUtils.RotateCameraAround(middlePosition, Vector3.up, 360, 30, Ease.Linear));
-        yield return new WaitForSeconds(30);
+        StartCoroutine(CameraUtils.RotateCameraAround(middlePosition, Vector3.up, -315, 4, Ease.Linear));
+        yield return new WaitForSeconds(4);
+        StartCoroutine(CameraUtils.MoveCamera(new Vector3(-8f, 6f, -1.5f), duration: 3, Ease.Linear));
+        yield return new WaitForSeconds(2);
+        StartCoroutine(SpawnCity(duration: 1));
+        yield return new WaitForSeconds(10);
 
         EditorApplication.isPlaying = false;
+    }
+
+    IEnumerator SpawnCity(float duration)
+    {
+        city2.transform.position = new Vector3(0, 0, -12);
+        Vector3 startScale = Vector3.one * 0.1f;
+        city2.transform.localScale = startScale;
+        Vector3 finalScale = Vector3.one;
+        float startTime = Time.time;
+
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            float scaleFactor = EaseUtils.EaseInOutCubic(t);
+            city2.transform.localScale = Vector3.Lerp(startScale, finalScale, scaleFactor);
+            yield return null;
+        }
+
+        city2.transform.localScale = finalScale;
     }
 }
