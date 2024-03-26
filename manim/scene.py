a1d24ab5b43f454d82c3490c9d03c0ea560d6674
markusenglund <markus.s.englund@gmail.com>
2024-03-26 16:48:26 +0100

    Manim hello world

---
+++ manim/scene.py
@@ -0,0 +1,23 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+
+
+class HelloLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r"\LaTeX", font_size=144)
+        self.play(Write(tex))
+
+
+class MathTeXDemo(Scene):
+    def construct(self):
+        rtarrow0 = MathTex(r"\xrightarrow{x^6y^8}", font_size=96)
+        rtarrow1 = Tex(r"$\xrightarrow{x^6y^8}$", font_size=96)
+
+        self.play(Write(VGroup(rtarrow0, rtarrow1).arrange(DOWN)))
