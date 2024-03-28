d606fbbfa9ed0e9a12d94a51e89f88753db69e61
markusenglund <markus.s.englund@gmail.com>
2024-03-28 17:19:33 +0100

    Fine tune speeds and so on

--- manim/scene.py
+++ manim/scene.py
@@ -41,17 +41,26 @@ class ListBusVariables(Scene):
 
 class NetValueCalculation(Scene):
     def construct(self):
-        equationStart = MathTex(r"netValueUber", r"=", r"tripValue", r"- fare", r"- (timeCost \cdot timeToArrival)", font_size=DEFAULT_FONT_SIZE)
-        equationStart[0].set_color(YELLOW)
+        equationStart = MathTex(r"netValueUber", r"=", r"tripValue", r"- fare", r"- (timeCost \cdot timeToArrival)", font_size=DEFAULT_FONT_SIZE).move_to(DOWN*2)
         equationWithValues = MathTex(r"=", r"\$31.10", r"- \$12.00", r"- (\$37.20/hr \cdot 0.35 \, hrs)", font_size=DEFAULT_FONT_SIZE)
         equationCombined = MathTex(r"=", r"\$31.10", r"- \$12.00", r"- \$13.02", font_size=DEFAULT_FONT_SIZE)
         equationSimplified = MathTex(r"=", r"\$6.08", font_size=DEFAULT_FONT_SIZE)
+        finalResultAtTheTop = MathTex(r"netValueUber", r"=", r"\$6.08", font_size=DEFAULT_FONT_SIZE).move_to(UP*3)
+        equationStart[0].set_color(YELLOW)
+        finalResultAtTheTop[0].set_color(YELLOW)
 
         equationWithValues.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
         equationCombined.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
         equationSimplified.next_to(equationWithValues, DOWN).align_to(equationStart[1], LEFT)
 
-        self.play(Write(equationStart))
+        self.play(Write(equationStart[0]))
+        self.play(Write(equationStart[1]))
+        self.wait(1)
+        self.play(Write(equationStart[2]))
+        self.wait(1)
+        self.play(Write(equationStart[3]))
+        self.wait(1)
+        self.play(Write(equationStart[4]))
         self.wait(1)
         self.play(LaggedStart(
             TransformFromCopy(equationStart[1], equationWithValues[0]),
@@ -66,34 +75,57 @@ class NetValueCalculation(Scene):
         self.wait(1)
         self.play(TransformFromCopy(equationCombined, equationSimplified))
         self.wait(1)
+        self.play(LaggedStart(
+            TransformFromCopy(equationStart[0], finalResultAtTheTop[0]),
+            TransformFromCopy(equationWithValues[0], finalResultAtTheTop[1]),
+            TransformFromCopy(equationSimplified[1], finalResultAtTheTop[2]),
+            lag_ratio=0.05,
+            run_time=1
+        ))
+        self.wait(1)
+        self.play(FadeOut(equationStart, equationWithValues, equationCombined, equationSimplified))
+        self.wait(1)
 
 class NetValueCalculationBus(Scene):
-   def construct(self):
-      equationStart = MathTex(r"netValueBus", r"=", r"tripValue", r"- fare", r"- (timeCost \cdot timeToArrival)", font_size=DEFAULT_FONT_SIZE)
-      equationStart[0].set_color(YELLOW)
-      equationWithValues = MathTex(r"=", r"\$31.10", r"- \$2.50", r"- (\$37.20/hr \cdot 0.67 \, hrs)", font_size=DEFAULT_FONT_SIZE)
-      equationCombined = MathTex(r"=", r"\$31.10", r"- \$2.50", r"- \$24.80", font_size=DEFAULT_FONT_SIZE)
-      equationSimplified = MathTex(r"=", r"\$3.80", font_size=DEFAULT_FONT_SIZE)
+    def construct(self):
+        equationStart = MathTex(r"netValueBus", r"=", r"tripValue", r"- fare", r"- (timeCost \cdot timeToArrival)", font_size=DEFAULT_FONT_SIZE).move_to(DOWN*2)
+        equationWithValues = MathTex(r"=", r"\$31.10", r"- \$2.50", r"- (\$37.20/hr \cdot 0.67 \, hrs)", font_size=DEFAULT_FONT_SIZE)
+        equationCombined = MathTex(r"=", r"\$31.10", r"- \$2.50", r"- \$24.80", font_size=DEFAULT_FONT_SIZE)
+        equationSimplified = MathTex(r"=", r"\$3.80", font_size=DEFAULT_FONT_SIZE)
 
-      equationWithValues.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
-      equationCombined.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
-      equationSimplified.next_to(equationWithValues, DOWN).align_to(equationStart[1], LEFT)
+        finalResultAtTheTop = MathTex(r"netValueBus", r"=", r"\$3.80", font_size=DEFAULT_FONT_SIZE).move_to(UP*2.2)
+        equationStart[0].set_color(YELLOW)
+        finalResultAtTheTop[0].set_color(YELLOW)
 
-      self.play(Write(equationStart))
-      self.wait(1)
-      self.play(LaggedStart(
-          TransformFromCopy(equationStart[1], equationWithValues[0]),
-          TransformFromCopy(equationStart[2], equationWithValues[1]),
-          TransformFromCopy(equationStart[3], equationWithValues[2]),
-          TransformFromCopy(equationStart[4], equationWithValues[3]),
-          lag_ratio=0.6,
-          run_time=3
-      ))
-      self.wait(1)
-      self.play(Transform(equationWithValues, equationCombined))
-      self.wait(1)
-      self.play(TransformFromCopy(equationCombined, equationSimplified))
-      self.wait(1)
+        equationWithValues.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
+        equationCombined.next_to(equationStart, DOWN).align_to(equationStart[1], LEFT)
+        equationSimplified.next_to(equationWithValues, DOWN).align_to(equationStart[1], LEFT)
+
+        self.play(Write(equationStart))
+        # self.wait(1)
+        self.play(LaggedStart(
+            TransformFromCopy(equationStart[1], equationWithValues[0]),
+            TransformFromCopy(equationStart[2], equationWithValues[1]),
+            TransformFromCopy(equationStart[3], equationWithValues[2]),
+            TransformFromCopy(equationStart[4], equationWithValues[3]),
+            lag_ratio=0.6,
+            run_time=1.5
+        ))
+        # self.wait(1)
+        self.play(Transform(equationWithValues, equationCombined))
+        # self.wait(1)
+        self.play(TransformFromCopy(equationCombined, equationSimplified))
+        self.wait(1)
+        self.play(LaggedStart(
+            TransformFromCopy(equationStart[0], finalResultAtTheTop[0]),
+            TransformFromCopy(equationWithValues[0], finalResultAtTheTop[1]),
+            TransformFromCopy(equationSimplified[1], finalResultAtTheTop[2]),
+            lag_ratio=0.05,
+            run_time=1
+        ))
+        self.wait(1)
+        self.play(FadeOut(equationStart, equationWithValues, equationCombined, equationSimplified))
+        self.wait(1)
 class SurplusCalculation(Scene):
     def construct(self):
         
