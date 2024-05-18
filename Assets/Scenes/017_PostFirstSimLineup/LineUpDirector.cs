54e2d307470ad3a36357d1494d699c5367a22456
markusenglund <markus.s.englund@gmail.com>
2024-05-18 12:12:25 +0200

    Make label offset better and easily customizable

--- Assets/Scenes/017_PostFirstSimLineup/LineUpDirector.cs
+++ Assets/Scenes/017_PostFirstSimLineup/LineUpDirector.cs
@@ -256,7 +256,7 @@ public class LineUpDirector : MonoBehaviour
         rejectedAverageLineTransform = Instantiate(averageLineTransform, canvas.transform);
         rejectedAverageDotTransform = Instantiate(averageDotTransform, canvas.transform);
         rejectedAverageLabelTransform = Instantiate(averageLabelTransform, canvas.transform);
-        yield return StartCoroutine(SpawnAverageLine(rejectedAverageLineTransform, rejectedAverageDotTransform, rejectedAverageLabelTransform, averageRejectedTimePreference, Color.red, -4.9f, new Vector3(0f, 0, 0.1f)));
+        yield return StartCoroutine(SpawnAverageLine(rejectedAverageLineTransform, rejectedAverageDotTransform, rejectedAverageLabelTransform, averageRejectedTimePreference, Color.red, -4.9f, new Vector3(-0.2f, 0, 0f)));
 
         yield return new WaitForSeconds(2f);
 
@@ -318,41 +318,12 @@ public class LineUpDirector : MonoBehaviour
         float uberAverageIncome = passengersWhoGotAnUber.Average(p => p.person.economicParameters.hourlyIncome);
         float rejectedAverageIncome = passengersWhoRejectedRideOffer.Average(p => p.person.economicParameters.hourlyIncome);
         float noOfferAverageIncome = passengersWhoDidNotReceiveRideOffer.Average(p => p.person.economicParameters.hourlyIncome);
-        StartCoroutine(MoveAverageLineToIncomeDistribution(uberAverageLineTransform, uberAverageDotTransform, uberAverageLabelTransform, uberAverageIncome));
-        StartCoroutine(MoveAverageLineToIncomeDistribution(rejectedAverageLineTransform, rejectedAverageDotTransform, rejectedAverageLabelTransform, rejectedAverageIncome));
-        StartCoroutine(MoveAverageLineToIncomeDistribution(noOfferAverageLineTransform, noOfferAverageDotTransform, noOfferAverageLabelTransform, noOfferAverageIncome));
+        StartCoroutine(MoveAverageLineToIncomeDistribution(uberAverageLineTransform, uberAverageDotTransform, uberAverageLabelTransform, uberAverageIncome, new Vector3(0.3f, 0, 0)));
+        StartCoroutine(MoveAverageLineToIncomeDistribution(rejectedAverageLineTransform, rejectedAverageDotTransform, rejectedAverageLabelTransform, rejectedAverageIncome, new Vector3(-0.3f, 0, 0)));
+        StartCoroutine(MoveAverageLineToIncomeDistribution(noOfferAverageLineTransform, noOfferAverageDotTransform, noOfferAverageLabelTransform, noOfferAverageIncome, new Vector3(0, 0, 0.1f)));
         yield return null;
     }
 
-    private IEnumerator MoveAverageLineToIncomeDistribution(Transform lineTransform, Transform dotTransform, Transform labelTransform, float averageIncome)
-    {
-        LineRenderer averageLine = lineTransform.GetComponent<LineRenderer>();
-        LineRenderer averageDot = dotTransform.GetComponent<LineRenderer>();
-
-        float duration = 1f;
-        Vector3 startPosition0 = averageLine.GetPosition(0);
-        Vector3 startPosition1 = averageLine.GetPosition(1);
-        float endPositionX = ConvertHourlyIncomeToLinePosition(averageIncome);
-        Vector3 endPosition0 = new Vector3(endPositionX, startPosition0.y, startPosition0.z);
-        Vector3 endPosition1 = new Vector3(endPositionX, startPosition1.y, startPosition1.z);
-        Vector3 labelStartPosition = labelTransform.position;
-        Vector3 labelEndPosition = labelStartPosition + endPosition0 - startPosition0;
-
-        float startTime = Time.time;
-        while (Time.time < startTime + duration)
-        {
-            float t = (Time.time - startTime) / duration;
-            float positionFactor = EaseUtils.EaseInOutQuadratic(t);
-            averageLine.SetPosition(0, Vector3.Lerp(startPosition0, endPosition0, positionFactor));
-            averageLine.SetPosition(1, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
-            averageDot.SetPosition(0, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
-            averageDot.SetPosition(1, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
-            labelTransform.position = Vector3.Lerp(labelStartPosition, labelEndPosition, positionFactor);
-            yield return null;
-        }
-        averageLine.SetPosition(0, endPosition0);
-        averageLine.SetPosition(1, endPosition1);
-    }
 
 
     private IEnumerator ChangeLabel(Transform label, string text)
@@ -453,6 +424,37 @@ public class LineUpDirector : MonoBehaviour
             averageUberDot.endColor = lineRendererColor;
             yield return null;
         }
+    }
+
+
+    private IEnumerator MoveAverageLineToIncomeDistribution(Transform lineTransform, Transform dotTransform, Transform labelTransform, float averageIncome, Vector3 labelOffset)
+    {
+        LineRenderer averageLine = lineTransform.GetComponent<LineRenderer>();
+        LineRenderer averageDot = dotTransform.GetComponent<LineRenderer>();
+
+        float duration = 1f;
+        Vector3 startPosition0 = averageLine.GetPosition(0);
+        Vector3 startPosition1 = averageLine.GetPosition(1);
+        float endPositionX = ConvertHourlyIncomeToLinePosition(averageIncome);
+        Vector3 endPosition0 = new Vector3(endPositionX, startPosition0.y, startPosition0.z);
+        Vector3 endPosition1 = new Vector3(endPositionX, startPosition1.y, startPosition1.z);
+        Vector3 labelStartPosition = labelTransform.position;
+        Vector3 labelEndPosition = endPosition1 + labelOffset;
+
+        float startTime = Time.time;
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            float positionFactor = EaseUtils.EaseInOutQuadratic(t);
+            averageLine.SetPosition(0, Vector3.Lerp(startPosition0, endPosition0, positionFactor));
+            averageLine.SetPosition(1, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
+            averageDot.SetPosition(0, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
+            averageDot.SetPosition(1, Vector3.Lerp(startPosition1, endPosition1, positionFactor));
+            labelTransform.position = Vector3.Lerp(labelStartPosition, labelEndPosition, positionFactor);
+            yield return null;
+        }
+        averageLine.SetPosition(0, endPosition0);
+        averageLine.SetPosition(1, endPosition1);
     }
 
     private float ConvertHourlyIncomeToLinePosition(float hourlyIncome)
