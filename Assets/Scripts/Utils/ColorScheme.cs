c60925ef043d273b9a8b198b87889e9e14ebbfc9
markusenglund <markus.s.englund@gmail.com>
2024-05-18 15:35:16 +0200

    Add legend

--- Assets/Scripts/Utils/ColorScheme.cs
+++ Assets/Scripts/Utils/ColorScheme.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 
 public class ColorScheme : MonoBehaviour
 {
-    public static Color blue = new Color32(25, 121, 255, 255);
+    public static Color blue = new Color32(0, 184, 255, 255);
     public static Color purple = new Color32(255, 51, 180, 255);
     public static Color lightGrey = new Color32(224, 224, 224, 255);
 
