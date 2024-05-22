c29bbbdf934f61a932241aa922b993271edad551
markusenglund <markus.s.englund@gmail.com>
2024-05-22 12:58:17 +0200

    Set up BRoll scene

---
+++ Assets/Scenes/006_BRollScene/BRollSceneDirector.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+
+public class BRollDirector : MonoBehaviour
+{
+    [SerializeField] private Transform cityPrefab;
+    [SerializeField] public SimulationSettings simSettings;
+    [SerializeField] public GraphSettings graphSettings;
+
+    Vector3 cameraStartPosition = new Vector3(3f, 1.8f, 0f);
+    Quaternion cameraStartRotation = Quaternion.Euler(90, 90, 0);
+    City city;
+    void Awake()
+    {
+        city = City.Create(cityPrefab, 0, 0, simSettings, graphSettings);
+        StartCoroutine(city.StartSimulation());
+
+        Time.captureFramerate = 60;
+
+    }
+
+    void Start()
+    {
+        Camera.main.transform.position = cameraStartPosition;
+        Camera.main.transform.rotation = cameraStartRotation;
+        Time.timeScale = 0.8f;
+        StartCoroutine(Scene());
+    }
+
+    IEnumerator Scene()
+    {
+        StartCoroutine(CameraUtils.MoveCamera(cameraStartPosition + new Vector3(0, 0, 9), duration: 25, Ease.Linear));
+        yield return new WaitForSeconds(25);
+        // StartCoroutine(CameraUtils.MoveCamera(new Vector3(-8f, 6f, -1.5f), duration: 3, Ease.Cubic));
+        // StartCoroutine(CameraUtils.RotateCameraAround(middlePosition, Vector3.up, -90, 3, Ease.Linear));
+
+        EditorApplication.isPlaying = false;
+    }
+}
