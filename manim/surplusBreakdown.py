6bd2949675936e5aa62906aa8df091b828ab8776
markusenglund <markus.s.englund@gmail.com>
2024-07-26 19:51:30 +0200

    Fix colors and labels

--- manim/surplusBreakdown.py
+++ manim/surplusBreakdown.py
@@ -1,12 +1,12 @@
 from manim import *
 
-DEFAULT_FONT_SIZE = 18
+DEFAULT_FONT_SIZE = 17
 Text.set_default(font="sans-serif", font_size=DEFAULT_FONT_SIZE)
 
 class SurplusBreakdown(Scene):
     def construct(self):
       self.camera.background_color = "#444444"
-      
+      orange = ORANGE
       # values=[0, 0, 0, 0, 0, 0, 0]
       final_values = [-0.24, 4.47, 4.94, 3.06, 2.61, -16.07, -2.61]
       bar_names = ["Total", "Waiting time", "Time sensitivity", "Income", "Substitute speed", "Fare", "# passengers"]      
@@ -27,7 +27,7 @@ class SurplusBreakdown(Scene):
             "label_constructor": Text,
             "include_ticks": False,
           },
-          bar_colors=[ORANGE, GREEN]
+          bar_colors=[ORANGE, GREEN, GREEN, GREEN, GREEN, ORANGE, ORANGE]
       ).shift(UP*0)
       yLabel = chart.get_y_axis_label(Text("Surplus difference ($)", font_size=26)).shift(LEFT*3.3 + DOWN*3.1).rotate(90 * DEGREES)
       heading = Text("Difference in surplus per passenger", font_size = 40).shift(UP*3.5)
@@ -39,7 +39,12 @@ class SurplusBreakdown(Scene):
       self.wait(2)
 
       self.play(chart.animate.change_bar_values(final_values), run_time=2)
-      barLabels = chart.get_bar_labels(font_size=24, label_constructor=Text)
+      def prefix_label(text):
+        if "-" not in text:
+          return Text("+$" + text)
+        return Text("-$" + text.replace("-", ""))
+
+      barLabels = chart.get_bar_labels(font_size=24, label_constructor=lambda text: prefix_label(text))
       self.play(FadeIn(barLabels))
       self.wait(3)
     
